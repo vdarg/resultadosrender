2ef24effe5ebb36e7cabfcec0f040e624da8597c
vda <127701893+vdarg@users.noreply.github.com>
2024-07-26 21:19:54 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/cargar_votos.html'));
+});
+
+app.get('/resultados', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/resultados.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
